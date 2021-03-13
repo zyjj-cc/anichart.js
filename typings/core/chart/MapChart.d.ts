33785af3ad4201ec38bc734fa70c9b88d1e2eb6a
jannchie <jannchie@gmail.com>
2021-03-13 18:47:20 +0900

    update: changelog

--- typings/core/chart/MapChart.d.ts
+++ typings/core/chart/MapChart.d.ts
@@ -4,6 +4,9 @@ import { Path } from "../component/Path";
 import { Stage } from "../Stage";
 import { BaseChart, BaseChartOptions } from "./BaseChart";
 interface MapChartOptions extends BaseChartOptions {
+    labelAlphaScale?: ScaleLinear<number, number, never>;
+    labelPadding?: number;
+    labelSize?: number;
     pathShadowBlur?: number;
     pathShadowColor?: string;
     useShadow?: boolean;
@@ -19,19 +22,23 @@ interface MapChartOptions extends BaseChartOptions {
     visualMap?: (t: number) => string;
     getMapId?: (id: string) => string;
     strokeStyle?: string;
-    defaultFill?: string;
+    defaultFill?: string | CanvasGradient | CanvasPattern;
+    noDataLabel?: string;
+    showLabel?: boolean;
 }
 export declare class MapChart extends BaseChart {
     geoGener: GeoPath<any, GeoPermissibleObjects>;
     pathMap: Map<string, string | null>;
     pathComponentMap: Map<string, Path>;
+    labelComponentMap: Map<string, Component>;
     projection: GeoProjection;
     map: any;
     mapIdField: string;
     visualMap: (t: number) => string;
+    noDataLabel: string | undefined | null;
     getMapId: (id: string) => string;
     strokeStyle: string;
-    defaultFill: string;
+    defaultFill: string | CanvasGradient | CanvasPattern;
     projectionType: "orthographic" | "natural" | "mercator" | "equirectangular";
     scale: ScaleLinear<number, number, never>;
     showGraticule: boolean;
@@ -40,6 +47,10 @@ export declare class MapChart extends BaseChart {
     pathShadowBlur: number;
     pathShadowColor: string | undefined;
     useShadow: boolean;
+    showLabel: boolean;
+    labelPadding: number;
+    labelSize: any;
+    labelAlphaScale: ScaleLinear<number, number, never>;
     constructor(options?: MapChartOptions);
     margin: {
         top: number;
@@ -51,6 +62,7 @@ export declare class MapChart extends BaseChart {
     wrapper: Component;
     private init;
     private initGeoPath;
+    labelFormat: (id: string) => string;
     private initPathMap;
     private initComps;
     getComponent(sec: number): Component;
